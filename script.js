54295b92b1a4d0d0098a0b6099e576340fc2aec3
leo011619 <leonard.ferraz@hotmail.com>
2021-10-20 15:09:55 -0300

    Feature: Criação do script do botão ver mais do resumo junto da sua estilização

---
+++ script.js
@@ -0,0 +1,17 @@
+//----------Função do Ver mais----------------------------------//
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "ver mais";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "ver menos";
+    moreText.style.display = "inline";
+  }
+}
